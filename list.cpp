fd822fc9d162ef44908841f04d108288a902d777
Oleksandra <oleksandra@gmail.com>
2025-05-23 19:05:38 +0300

    First commit

---
+++ list.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+
+void insert_front(std::int32_t value);
+ void insert_back(std::int32_t value);
+ void erase_front();
+ void erase_back();
+  bool empty();
+  std::size_t size();
